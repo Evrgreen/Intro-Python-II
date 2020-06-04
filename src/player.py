98677742304bdf9260004327e97903dc34b1dd0d
evrgreen <rcarsten06@gmail.com>
2020-06-04 08:02:24 -0600

    setup player, room class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+
+    def move(self, direction):
+        if f'{direction}_to' in dir(self.location):
+            move_to = getattr(self.location, f'{direction}_to')
+            self.location = move_to
+          
